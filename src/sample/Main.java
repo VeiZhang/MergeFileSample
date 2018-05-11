42cfd56945a77a7e1a0ea9b4e00227315261c5e7
master <690393541@qq.com>
2018-05-11 18:15:02 +0800

    添加布局

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+		primaryStage.setTitle("合并文件工具");
+		primaryStage.setScene(new Scene(root, 520, 300));
+		/**
+		 * 固定宽高
+		 */
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 }
