a78d34f60220392faaf5d85935699e5bde0bd4c2
master <690393541@qq.com>
2018-05-14 20:11:02 +0800

    完成拖拽文件获取路径

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,18 +5,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sample.utils.FileDragEventHandler;
 
 public class Controller implements Initializable
 {
+	private static final String OTT_DES = "OTT files (*.zip)";
+	private static final String OTT_EXTENSION = "*.zip";
+	private static final String TV_DES = "TV files (*.bin)";
+	private static final String TV_EXTENSION = "*.bin";
+
 	private Stage mPrimaryStage;
 
 	@FXML
@@ -45,31 +49,36 @@ public class Controller implements Initializable
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-		ottFilePath.setOnDragOver(new EventHandler<DragEvent>()
-		{
-			@Override
-			public void handle(DragEvent event)
-			{
-				/**
-				 * 显示文件拖拽的状态，否则图标是禁止的
-				 */
-				event.acceptTransferModes(TransferMode.ANY);
-			}
-		});
+		/**
+		 * 也可以直接使用fxml的方式
+		 */
+		ottFilePath.setOnDragOver(new FileDragEventHandler(OTT_EXTENSION));
+		tvFilePath.setOnDragOver(new FileDragEventHandler(TV_EXTENSION));
 	}
 
 	public void ottDragDropEvent(DragEvent event)
 	{
+		ottFilePath.setText(getDragDropFilePath(event));
+	}
+
+	private String getDragDropFilePath(DragEvent event)
+	{
 		Dragboard dragboard = event.getDragboard();
 		if (dragboard.hasFiles())
 		{
-			ottFilePath.setText(dragboard.getFiles().get(0).getPath());
+			return dragboard.getFiles().get(0).getPath();
 		}
+		return null;
+	}
+
+	public void tvDragDropEvent(DragEvent event)
+	{
+		tvFilePath.setText(getDragDropFilePath(event));
 	}
 
 	public void ottSelectEvent(ActionEvent event)
 	{
-		File file = getChooserFile("OTT files (*.zip)", "*.zip");
+		File file = getChooserFile(OTT_DES, OTT_EXTENSION);
 		if (file != null)
 		{
 			ottFilePath.setText(file.getPath());
@@ -86,7 +95,7 @@ public class Controller implements Initializable
 
 	public void tvSelectEvent(ActionEvent event)
 	{
-		File file = getChooserFile("TV files (*.bin)", "*.bin");
+		File file = getChooserFile(TV_DES, TV_EXTENSION);
 		if (file != null)
 		{
 			tvFilePath.setText(file.getPath());
---
+++ src/sample/utils/FileDragEventHandler.java
@@ -0,0 +1,41 @@
+package sample.utils;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2018/5/14
+ *     desc   :
+ * </pre>
+ */
+
+public class FileDragEventHandler implements EventHandler<DragEvent>
+{
+	private String mExtension = null;
+
+	public FileDragEventHandler(String extension)
+	{
+		mExtension = extension.replace("*", "");
+	}
+
+	@Override
+	public void handle(DragEvent event)
+	{
+		Dragboard dragboard = event.getDragboard();
+		if (dragboard.hasFiles())
+		{
+			if (dragboard.getFiles().get(0).getPath().endsWith(mExtension))
+			{
+				/**
+				 * 显示文件拖拽的状态，否则图标是禁止的
+				 */
+				event.acceptTransferModes(TransferMode.ANY);
+			}
+		}
+	}
+}
